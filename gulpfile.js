f7c3a4220a2b4ef34a9450ba5f2f065a47c18c33
el-polar <53009640+el-polar@users.noreply.github.com>
2022-09-22 12:21:47 -0500

    Watch Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest } = require("gulp");
+const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 /* 
@@ -10,8 +10,11 @@ const sass = require("gulp-sass")(require("sass"));
 */
 function css(done) {
   src("src/scss/app.scss").pipe(sass()).pipe(dest("build/css"));
-  
+
   done();
 }
 
-exports.css = css;
+function dev() {
+  watch("src/scss/app.scss", css);
+}
+exports.dev = dev;
